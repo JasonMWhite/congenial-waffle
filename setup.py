829b3a375fff2830537a0b4462195b6e8e0074ee
Jason White <jason.white@shopify.com>
2018-06-09 00:29:13 -0400

    project scaffolding

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='congenial-waffle',
+    version='0.0.1',
+    author='Jason White',
+    author_email='actinolite.jw@gmail.com',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+)
