e6d78035bbc99b6a2bd08ff2c9cadd2372bc7dd4
Jason White <jason.white@shopify.com>
2018-07-02 23:54:06 -0400

    use tip of master for pylint

--- src/waffle/logger.py
+++ src/waffle/logger.py
@@ -5,12 +5,12 @@ def create_logger() -> logging.Logger:
     log = logging.getLogger(__name__)
     log.setLevel(logging.DEBUG)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
+    handler.setFormatter(formatter)
 
-    log.addHandler(ch)
+    log.addHandler(handler)
     return log
 
 
